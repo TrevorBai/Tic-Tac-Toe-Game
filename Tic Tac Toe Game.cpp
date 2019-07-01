6179087b404abb400016b755b3db2d9096b9ffb1
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2019-06-30 21:47:59 -0400

    Add files via upload
    Refactored variables and functions based on naming conversion

--- Tic Tac Toe Game.cpp
+++ Tic Tac Toe Game.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-char square[10] = { 'o','1','2','3','4','5','6','7','8','9' };
+char gameBoardGridNumber[10] = { 'o','1','2','3','4','5','6','7','8','9' };
+// char 'o' is a dummy char so that gameBoardGridNumber[n] = n
 
-int checkwin();
-void board();
+int checkIfAnyPlayerMeetsWinningCriteria();
+void drawGameBoardAtConsole();
 
 /*******************************************************************
-	 FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+	 FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS LEGEND
 ********************************************************************/
 
-void board()
+void drawGameBoardAtConsole()
 {
 	system("cls");
 	cout << "\n\n\tTic Tac Toe\n\n";
@@ -19,17 +20,17 @@ void board()
 	cout << endl;
 
 	cout << "     |     |     " << endl;
-	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+	cout << "  " << gameBoardGridNumber[1] << "  |  " << gameBoardGridNumber[2] << "  |  " << gameBoardGridNumber[3] << endl;
 
 	cout << "_____|_____|_____" << endl;
 	cout << "     |     |     " << endl;
 
-	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+	cout << "  " << gameBoardGridNumber[4] << "  |  " << gameBoardGridNumber[5] << "  |  " << gameBoardGridNumber[6] << endl;
 
 	cout << "_____|_____|_____" << endl;
 	cout << "     |     |     " << endl;
 
-	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+	cout << "  " << gameBoardGridNumber[7] << "  |  " << gameBoardGridNumber[8] << "  |  " << gameBoardGridNumber[9] << endl;
 
 	cout << "     |     |     " << endl << endl;
 }
@@ -41,35 +42,35 @@ void board()
 	O GAME IS OVER AND NO RESULT (GAME DRAW)
 **********************************************/
 
-int checkwin()
+int checkIfAnyPlayerMeetsWinningCriteria()
 {
-	if (square[1] == square[2] && square[2] == square[3])
+	if (gameBoardGridNumber[1] == gameBoardGridNumber[2] && gameBoardGridNumber[2] == gameBoardGridNumber[3])
 
 		return 1;
-	else if (square[4] == square[5] && square[5] == square[6])
+	else if (gameBoardGridNumber[4] == gameBoardGridNumber[5] && gameBoardGridNumber[5] == gameBoardGridNumber[6])
 
 		return 1;
-	else if (square[7] == square[8] && square[8] == square[9])
+	else if (gameBoardGridNumber[7] == gameBoardGridNumber[8] && gameBoardGridNumber[8] == gameBoardGridNumber[9])
 
 		return 1;
-	else if (square[1] == square[4] && square[4] == square[7])
+	else if (gameBoardGridNumber[1] == gameBoardGridNumber[4] && gameBoardGridNumber[4] == gameBoardGridNumber[7])
 
 		return 1;
-	else if (square[2] == square[5] && square[5] == square[8])
+	else if (gameBoardGridNumber[2] == gameBoardGridNumber[5] && gameBoardGridNumber[5] == gameBoardGridNumber[8])
 
 		return 1;
-	else if (square[3] == square[6] && square[6] == square[9])
+	else if (gameBoardGridNumber[3] == gameBoardGridNumber[6] && gameBoardGridNumber[6] == gameBoardGridNumber[9])
 
 		return 1;
-	else if (square[1] == square[5] && square[5] == square[9])
+	else if (gameBoardGridNumber[1] == gameBoardGridNumber[5] && gameBoardGridNumber[5] == gameBoardGridNumber[9])
 
 		return 1;
-	else if (square[3] == square[5] && square[5] == square[7])
+	else if (gameBoardGridNumber[3] == gameBoardGridNumber[5] && gameBoardGridNumber[5] == gameBoardGridNumber[7])
 
 		return 1;
-	else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
-		&& square[4] != '4' && square[5] != '5' && square[6] != '6'
-		&& square[7] != '7' && square[8] != '8' && square[9] != '9')
+	else if (gameBoardGridNumber[1] != '1' && gameBoardGridNumber[2] != '2' && gameBoardGridNumber[3] != '3'
+		&& gameBoardGridNumber[4] != '4' && gameBoardGridNumber[5] != '5' && gameBoardGridNumber[6] != '6'
+		&& gameBoardGridNumber[7] != '7' && gameBoardGridNumber[8] != '8' && gameBoardGridNumber[9] != '9')
 
 		return 0;
 	else
@@ -78,64 +79,64 @@ int checkwin()
 
 int main()
 {
-	int player = 1, i, choice;
+	int indexPlayer = 1, winningCriteriaBit, whereToPlaceChess;
 
-	char mark;
+	char placeChess;
 
 	do
 	{
-		board();
-		player = (player % 2) ? 1 : 2; // player is aucillating with either 1 or 2
+		drawGameBoardAtConsole();
+		indexPlayer = (indexPlayer % 2) ? 1 : 2; // player is aucillating with either 1 or 2
 
-		cout << "Player " << player << ", enter a number:  ";
-		cin >> choice;
+		cout << "Player " << indexPlayer << ", enter a number:  ";
+		cin >> whereToPlaceChess;
 
-		mark = (player == 1) ? 'X' : 'O';
+		placeChess = (indexPlayer == 1) ? 'X' : 'O';
 
-		if (choice == 1 && square[1] == '1')
+		if (whereToPlaceChess == 1 && gameBoardGridNumber[1] == '1')
 
-			square[1] = mark;
-		else if (choice == 2 && square[2] == '2')
+			gameBoardGridNumber[1] = placeChess;
+		else if (whereToPlaceChess == 2 && gameBoardGridNumber[2] == '2')
 
-			square[2] = mark;
-		else if (choice == 3 && square[3] == '3')
+			gameBoardGridNumber[2] = placeChess;
+		else if (whereToPlaceChess == 3 && gameBoardGridNumber[3] == '3')
 
-			square[3] = mark;
-		else if (choice == 4 && square[4] == '4')
+			gameBoardGridNumber[3] = placeChess;
+		else if (whereToPlaceChess == 4 && gameBoardGridNumber[4] == '4')
 
-			square[4] = mark;
-		else if (choice == 5 && square[5] == '5')
+			gameBoardGridNumber[4] = placeChess;
+		else if (whereToPlaceChess == 5 && gameBoardGridNumber[5] == '5')
 
-			square[5] = mark;
-		else if (choice == 6 && square[6] == '6')
+			gameBoardGridNumber[5] = placeChess;
+		else if (whereToPlaceChess == 6 && gameBoardGridNumber[6] == '6')
 
-			square[6] = mark;
-		else if (choice == 7 && square[7] == '7')
+			gameBoardGridNumber[6] = placeChess;
+		else if (whereToPlaceChess == 7 && gameBoardGridNumber[7] == '7')
 
-			square[7] = mark;
-		else if (choice == 8 && square[8] == '8')
+			gameBoardGridNumber[7] = placeChess;
+		else if (whereToPlaceChess == 8 && gameBoardGridNumber[8] == '8')
 
-			square[8] = mark;
-		else if (choice == 9 && square[9] == '9')
+			gameBoardGridNumber[8] = placeChess;
+		else if (whereToPlaceChess == 9 && gameBoardGridNumber[9] == '9')
 
-			square[9] = mark;
+			gameBoardGridNumber[9] = placeChess;
 		else
 		{
 			cout << "Invalid move";
 			//system("pause");
-
-			player--;
+			indexPlayer--; // post-decrement returns value BEFORE calculation
 			cin.ignore();
 			cin.get();
 		}
 
-		i = checkwin();
-		player++;
-	} while (i == -1);
-	
-	board();
-	if (i == 1)
-		cout << "==>\aPlayer " << --player << " win "; // \a is an alarm
+		winningCriteriaBit = checkIfAnyPlayerMeetsWinningCriteria();
+		indexPlayer++;
+	} while (winningCriteriaBit == -1);
+
+	drawGameBoardAtConsole();
+	if (winningCriteriaBit == 1)
+		cout << "==>\aPlayer " << --indexPlayer << " win "; // \a is an alarm
+		// pre-decrement returns value AFTER calculation
 	else
 		cout << "==>\aGame draw";
 
